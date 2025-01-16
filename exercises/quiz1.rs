35336ced1005b95d0ae8c54b652f1ff5fbfc59b2
delxs <delxs@tutanota.com>
2025-01-16 21:54:04 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nums:i32) -> i32 {
+    if nums > 40 {
+        nums
+    } else {
+        2 * nums
+    }
+}
 
 // Don't modify this function!
 #[test]
